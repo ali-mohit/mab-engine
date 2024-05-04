2f1d2732ab02d3ebbe1346a649b2a1ec30de2115
Ali Mohit <ali.mohit.sbu@gmail.com>
2024-05-04 19:55:03 +0330

    implementing Orthogonal Camera

--- MABEngine/src/MABEngine/Renderer/EngineRenderer.h
+++ MABEngine/src/MABEngine/Renderer/EngineRenderer.h
@@ -4,7 +4,9 @@
 #include "MABEngine/Renderer/RendererAPI.h"
 #include "MABEngine/Renderer/RenderAPIType.h"
 #include "MABEngine/Renderer/VertexArray.h"
-
+#include "MABEngine/Renderer/OrthographicCamera.h"
+#include "MABEngine/Renderer/SceneDataType.h"
+#include "MABEngine/Renderer/Shader.h"
 
 namespace MABEngine {
 
@@ -13,12 +15,14 @@ namespace MABEngine {
 		class MABENGINE_API EngineRenderer
 		{
 		public:
-			static void BeginScene();
+			static void BeginScene(OrthographicCamera& camera);
 			static void EndScene();
 
-			static void Submit(const std::shared_ptr<VertexArray>& vertexArray);
+			static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
 
 			inline static RendererAPIType GetCurrentAPI() { return RendererAPI::GetAPI(); }
+		private:
+			static SceneDataType* m_SceneData;
 		};
 
 	}
